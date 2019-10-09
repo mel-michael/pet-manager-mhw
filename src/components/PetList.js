0ee7ba135b5e4d0494dbf8538c0d7ecbcb80efd0
Mel Michael <mel.michael007@gmail.com>
2019-10-09 12:13:01 +0100

    Implement search functionality

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import Pet from './Pet';
+import { IoIosSearch } from "react-icons/io";
+
 import { animals } from '../data'
 import '../App.css';
 
-const PetList = () => (
-  <div className="App">
-    <header className="App-header">
-      <input type="text" placeholder="Search Pets..." className="search_input" />
-    </header>
-    <div className="pet_wrapper">
-      <ul className="pet_list">
-        {
-          animals.map(animal => (<li key={animal.id}><Pet pet={animal} /></li>))
-        }
-      </ul>
-    </div>
-  </div>
-);
+class PetList extends PureComponent {
+  state = {
+    searchValue: '',
+    animalList: [],
+    filtered: [],
+    isSearched: false,
+  }
+
+  componentDidMount() {
+    this.setState({ animalList: animals, filtered: animals })
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchValue: e.target.value, isSearched: false });
+  }
+  
+  handleSearch = () =>  {
+    const { searchValue, animalList } = this.state;
+    const result = animalList.filter(animal => animal.name.toLowerCase().includes(searchValue.toLowerCase()))
+    this.setState({ isSearched: true, filtered: result })
+  }
+
+  render() {
+    const { searchValue, animalList, isSearched, filtered } = this.state;
+    let List;
+
+    if(isSearched) {
+      List = filtered.map(animal => (<li key={animal.id}><Pet pet={animal} /></li>))
+    } else {
+      List = animalList.map(animal => (<li key={animal.id}><Pet pet={animal} /></li>))
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="text" placeholder="Search Pets..."
+            className="search_input" value={searchValue}
+            onChange={this.handleChange}
+          />
+          <span className="search_icon" onClick={this.handleSearch}><IoIosSearch /></span>
+        </header>
+        <div className="pet_wrapper">
+          <ul className="pet_list">
+            {List}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PetList;
